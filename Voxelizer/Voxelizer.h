ac59c3e0e80fef3c4b013f76dbfbdea9d3219f12
NickPavini <nickpavini@gmail.com>
2018-03-26 20:00:20 -0700

    updates

--- Voxelizer/Voxelizer.h
+++ Voxelizer/Voxelizer.h
@@ -14,6 +14,7 @@ class Voxelizer
 	  uint32_t numOfVoxels; //grid dimensions determined by numOfVoxels, i.e numOfVoxels length, width, height in grid since cube
 	  double voxelSize;   //will be length width and height of each voxel
 	  double x_transform, y_transform, z_transform; //transform entire molecule onto pos xy plane, 1st octet
+    bool voxelized_flag;
 
 	  void setGrid(Atom * const &, uint32_t); //finds transform to move all points into octet 1 (x,y,z > 0), and cube dimensions in numOfVoxels
 	  void populateGrid(Atom * const &, uint32_t); //will go through and populate protons, neutrons and electrons
