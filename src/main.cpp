0a9310aba5774495eb80bf98b0bd06540a365c0f
nickpavini <nickpavini@gmail.com>
2018-05-23 12:31:14 -0700

    Structured main.cpp such that control structure has a path to go to combining 2 molecule spaces. The combination of 2 molecule spaces has yet to be implemented.

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,59 @@
 #include "Voxelizer.h"
 
-void createActiveSite(const std::string path,const double voxelSize); //creates a new active site
-void addMolToAnActiveSite(const std::string path); //adds the specified molfile to the specified active site
-char getUserChoice(); //returns user choice to add to existing active site
+void createMoleculeSpace(const std::string path,const double voxelSize); //creates a new active site
+void molWithMolecule(const std::string path); //add mol file to molecule space
+void moleculeWithMolecule(const std::string path); //combines 2 molecule spaces
+double getVoxelSize();
+std::string getMolFilePath();
+std::string getMoleculePath();
 
 //takes in a molFile path with filename and a voxelSize in angstrums, Voxelizes it and outputs to json...also can take a json file of an active site and a new molFile path to add to it
 int main(int argc, char** argv)
 {
   std::string path;
   double voxelSize = 0.0;
-  char addingToExisting = 0;
+  char addingToExisting = 0, combiningSpaces = 0; //used to tell if creating new space, space with mol or sapce with space
 
   if(argc == 1) //user is running program without passing command line argument
   {
-    addingToExisting = getUserChoice(); //find out if there is a new or existing active site
+    int choice = 0;
+    bool done = false;
 
-    if(addingToExisting == 'y' || addingToExisting == 'Y') {    //existing active site
-      std::string temp;
-      std::cout << "Specify the path and file name to an active-site json file you would like to add to.\n";
-      std::cin >> temp;
+    while(!done)
+    {
+      std::cout << "1. Create a new molecule space\n";
+      std::cout << "2. Add a mol file to a molecule space.\n";
+      std::cout << "3. Combine 2 molecule spaces.\n\n";
+      std::cout << "How would you like to use the Voxelizer? Enter 1-3 as your selection.\n";
 
-      path += (temp + " "); //"active-site " active site path followed by space seperator
+      std::cin >> choice;
 
-      std::cout << "Specify the path and file name to a molFile that you would like to add to the active site. \nNote: Acitve site should be larger than added molecule.\n";
-      std::cin >> temp;  //add on molFile path
+      switch(choice)
+      {
+        case 1:
+          path = getMolFilePath();  //get the molFile path with filename from user
+          voxelSize = getVoxelSize(); //get the voxel size from user
+          done = true;
+          break;
 
-      path += temp; //"active-site molFile"
-    }
-    else {  //new active site
-      //get the molFile path with filename from user
-      std::cout << "Specify the path and file name to a molfile you would like to represent.\n";
-      std::cin >> path;
+        case 2: //path = "molecule mol"
+          path = getMoleculePath();
+          path += " ";
+          path += getMolFilePath();
+          addingToExisting = 'y';
+          done = true;
+          break;
 
-      //get the voxel size from user
-      std::cout << "Specify the Voxel Size in angstrums.\n";
-      std::cin >> voxelSize;
+        case 3: //path = "molecule molecule"
+          path = getMoleculePath();
+          path += " ";
+          path += getMoleculePath();
+          combiningSpaces = 'y';
+          done = true;
+          break;
+        default:
+          break;
+      }
     }
   }
   else if(argc == 3) //user is running with command line arguments, need to get file extension and check if json or mol
@@ -42,48 +61,64 @@ int main(int argc, char** argv)
     path = argv[1]; //path to active site or molFile path
 
     if(path.find(".json") != std::string::npos) {  //.json found..so argv[1] = active site path and argv[2] = molFile path
-      addingToExisting = 'y'; //adding to an existing molfile
+      if(std::string(argv[2]).find(".json") != std::string::npos) { //second file is molecule space, so combining 2 molecule spaces
+        combiningSpaces = 'y'; //combining 2 molecule spaces
+      }
+      else {
+        addingToExisting = 'y'; //adding to an existing molecule space;
+      }
+
       path += " ";
-      path += argv[2]; //path now holds "active-site-path molFile-path" with space in the middle for seperator
+      path += argv[2]; //path now holds "active-site-path molFile-path" or  "active-site-path active-site-path" with space in the middle for seperator
     }
     else if(path.find(".mol") != std::string::npos) {  //.mol found
-      addingToExisting = 'n'; //adding to an existing molfile
       voxelSize = std::atof(argv[2]);
     }
   }
-  else {
-    std::cout << "error in main - Too many command line arguments given to voxelizer program.\n";
+  else
+  {
+    std::cout << "error in main - Too many or too few command line arguments given to voxelizer program.\n";
     return 0;
   }
 
   //path at this point either is the molFile path, or then active site path, a space, and then the molFile path
-  if(addingToExisting == 'n' || addingToExisting == 'N')   //new active-site
-    createActiveSite(path, voxelSize);
-  else //adding to existing, and we are sure addingToExisting == either y || n or their respective capitals
-    addMolToAnActiveSite(path);
-
+  if(addingToExisting == 'y' || addingToExisting == 'Y')
+    molWithMolecule(path); //combine molfile to a molecule space
+  else if(combiningSpaces == 'y' || combiningSpaces == 'Y')  //new active-sitengSpaces == 'y' || combiningSpaces == 'Y')
+    moleculeWithMolecule(path); //combine 2 molecule spaces
+  else
+    createMoleculeSpace(path, voxelSize); //new active-site
   return 0;
 }
 
+//------------------------------------------------------------------------------
+//functions
 
-
-
-
-
-char getUserChoice()
+double getVoxelSize()
 {
-  char c;
-  //find out if we are adding to an active-site or a new active-site
-  do {
-    std::cout << "Are you adding to an existing Active-Site? (y or n)\n";
-    std::cin >> c;
-  } while(c != 'y' && c != 'Y' &&  //continue looping while it doesnt equal any of the following
-          c != 'n' && c != 'N');
-
-  return c;
+  double size;
+  std::cout << "Specify the Voxel Size in angstrums.\n";
+  std::cin >> size;
+  return size;
 }
 
-void createActiveSite(const std::string path, const double voxelSize)
+std::string getMolFilePath() //path to molFile
+{
+  std::string rtrn;
+  std::cout << "Specify the path and file name to a molfile you would like to represent.\n";
+  std::cin >> rtrn;
+  return rtrn;
+}
+
+std::string getMoleculePath() //path to molecule space
+{
+  std::string rtrn;
+  std::cout << "Specify the path and file name to a molecule space you would like to add t.\n";
+  std::cin >> rtrn;
+  return rtrn;
+}
+
+void createMoleculeSpace(const std::string path, const double voxelSize)
 {
   //by this point we have what was given by the user and now need to verify data integrity
   //get directory and filename form the user
@@ -116,7 +151,7 @@ void createActiveSite(const std::string path, const double voxelSize)
   v.exportJSON();
 }
 
-void addMolToAnActiveSite(const std::string path) //path contains "active-site molFile"
+void molWithMolecule(const std::string path) //path contains "active-site molFile"
 {
   std::string activeSitePath, molFilePath, molFileDirectory, molFileName;
   Voxelizer v; //Voxelizer object to hold active site and new molecule
@@ -151,4 +186,9 @@ void addMolToAnActiveSite(const std::string path) //path contains "active-site m
     std::cout << e << '\n';
     return;
   }
+}
+
+void moleculeWithMolecule(const std::string  path) //path contains 2 molcule spaces
+{
+  std::cout << "combining spaces.\n" << path << '\n';
 }
